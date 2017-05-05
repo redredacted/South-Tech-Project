66218703da049c9eae87590cfa5d5ffa13d61217
Personal Question <pquestion@outlook.com>
2017-05-05 13:59:28 -0500

    Created asp.net project, created some tables, created an entity data model.

---
+++ South-Tech-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(South_Tech_Project.Startup))]
+namespace South_Tech_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
